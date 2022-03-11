5fd8f74cb8f9d2ba12d141002a5edb43eaee4385
Blake-Foust <r10c7rt14@hotmail.com>
2022-03-11 05:29:45 -0500

    unstable game push

--- GameCode/Game.cpp
+++ GameCode/Game.cpp
@@ -1,6 +1,8 @@
 #include "Game.hpp"
 
-Game::Game():mWindow(sf::VideoMode(640,480), "SFML Application"),
+
+//Constructor
+Game::Game():mWindow(sf::VideoMode(1200,800), "SFML Application"),
 	     mPlayer()
 {
 	mPlayer.setRadius(40.f);
@@ -8,41 +10,101 @@ Game::Game():mWindow(sf::VideoMode(640,480), "SFML Application"),
 	mPlayer.setFillColor(sf::Color::Cyan);
 };
 
+//Destructor
 Game::~Game()
 {
 };
 
-//Handls user input
+//Handles user input
 void Game::processEvents()
 {
 	sf::Event event;
 	while(mWindow.pollEvent(event))
 	{
-		if(event.type == sf::Event::Closed)
-			mWindow.close();
+		switch(event.type)
+		{
+			case sf::Event::KeyPressed:
+				handlePlayerInput(event.key.code, true);
+				break;
+			case sf::Event::KeyReleased:
+				handlePlayerInput(event.key.code, false);
+				break;
+			case sf::Event::Closed:
+				mWindow.close();
+				break;
+		}
 	}
 };
 
-//Hanlds game logic
-void Game::update()
+//Handles game logic
+void Game::update(sf::Time deltaTime)
 {
+	sf::Vector2f movement(0.f,0.f);
+	if(this->mMovingUp)
+		movement.y -= this->playerSpeed;
+	if(this->mMovingDown)
+		movement.y += this->playerSpeed;
+	if(this->mMovingLeft)
+		movement.x -= this->playerSpeed;
+	if(this->mMovingRight)
+		movement.x += this->playerSpeed;
+	
+	mPlayer.move(movement);
 };
 
 //Renders game to screen
 void Game::render()
 {
+	sf::Texture texture;
+	if(!texture.loadFromFile("christmascard.png"))
+	{
+		mWindow.close();
+	}
+	sf::Sprite sprite(texture);
+	sprite.setPosition(100.f,100.f);
 	mWindow.clear();
-	mWindow.draw(mPlayer);
+	mWindow.draw(sprite);
 	mWindow.display();
 };
 
 //Functions
 void Game::run()
 {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	mWindow.setVerticalSyncEnabled(true);
 	while(mWindow.isOpen())
 	{
-		processEvents();
-		update();
+		timeSinceLastUpdate += clock.restart();
+		while(timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents();
+			update(TimePerFrame);
+
+		}
 		render();
+
 	}
+};
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool is_Pressed)
+{
+	switch (key)
+	{
+	case sf::Keyboard::W:
+		this->mMovingUp = is_Pressed;
+		break;
+	case sf::Keyboard::D:
+		this->mMovingRight = is_Pressed;
+		break;
+	case sf::Keyboard::A:
+		this->mMovingLeft = is_Pressed;
+		break;
+	case sf::Keyboard::S:
+		this->mMovingDown = is_Pressed;
+		break;
+	default:
+		break;
+	};
 };
--- GameCode/Game.hpp
+++ GameCode/Game.hpp
@@ -1,8 +1,18 @@
 #ifndef  GAME_H
 #define GAME_H
 
+//SFML Library
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Window.hpp>
+
+//Libaries
+#include <iostream>
+
+
+#define logl(x) std::cout << x << std::endl;
 
 class Game
 {
@@ -11,7 +21,7 @@ private:
 	sf::CircleShape  mPlayer;	
 private:
 	void processEvents();
-	void update();
+	void update(sf::Time);
 	void render();
 
 public:
@@ -21,6 +31,15 @@ public:
 
 	//Functions
 	void run();
+	void handlePlayerInput(sf::Keyboard::Key, bool);
+
+	//Variables
+	sf::Time TimePerFrame = sf::seconds(1.f/60.f);
+	bool mMovingUp = false;
+	bool mMovingDown = false;
+	bool mMovingRight = false;
+	bool mMovingLeft = false;
+	float playerSpeed = 1.0;
 
 };
 
